3df4e6a141a452f8939fc8e01627d56cc4e417b9
VietAnh <vietanhtb057@gmail.com>
2025-09-22 23:47:28 +0700

    Update code: Refactor summarizer.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from utils.text_utils import chunk_text
 from utils.file_utils import save_to_json
 
 URLs = ['https://vietnamnet.vn/dam-phan-nga-ukraine-ban-co-thu-nghiem-cho-trat-tu-the-gioi-2403267.html',
-       'https://vietnamnet.vn/hoc-tieng-anh-nhu-ngon-ngu-thu-hai-truong-day-ca-van-the-duc-bang-tieng-anh-2444589.html']
+       'https://vietnamnet.vn/hoc-tieng-anh-nhu-ngon-ngu-thu-hai-truong-day-ca-van-the-duc-bang-tieng-anh-2444589.html',
+       'https://vietnamnet.vn/danh-sach-8-doan-tuyen-cao-toc-duoc-de-nghi-phan-lan-thay-doi-toc-do-xe-chay-2444459.html']
 
 today_news = {}
 for URL in URLs:
@@ -15,7 +16,7 @@ for URL in URLs:
         'title': title,
         'main_content': main_content 
     }
-    
+
 print(today_news)
 save_to_json(today_news)
 
